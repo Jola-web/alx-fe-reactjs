62e442e22dccb54d64e8b2ea4f16aed9a10abd69
Jola-ade <ayilarajolaade@gmail.com>
2025-08-28 01:29:52 +0100

    Added Header, MainContent, and Footer components

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -4,6 +4,10 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+import Header from './Header';
+import MainContent from './MainContent';
+import Footer from './Footer';
+
 function App() {
   const [count, setCount] = useState(0)
 
@@ -11,6 +15,9 @@ function App() {
     <>
       <WelcomeMessage />
       <div>
+        <Header />
+        <MainContent />
+        <Footer />
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
