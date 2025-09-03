b9fd78d3d05928790662f005b5a727fe5720e70d
Jola-ade <ayilarajolaade@gmail.com>
2025-09-04 00:45:18 +0100

    Refactor UserProfile to useContext instead of props

--- alx-react-app-props/src/components/UserProfile.jsx
+++ alx-react-app-props/src/components/UserProfile.jsx
@@ -1,10 +1,14 @@
-// src/components/UserProfile.jsx
-const UserProfile = (props) => {
+import { useContext } from "react";
+import UserContext from "./UserContext";
+
+const UserProfile = () => {
+  const userData = useContext(UserContext);
+
   return (
     <div className="user-profile">
-      <h2>{props.name}</h2>
-      <p>Age: {props.age}</p>
-      <p>Bio: {props.bio}</p>
+      <h2>{userData.name}</h2>
+      <p>Age: {userData.age}</p>
+      <p>Bio: {userData.bio}</p>
     </div>
   );
 };
