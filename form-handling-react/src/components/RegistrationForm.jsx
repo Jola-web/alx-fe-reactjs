878e8570543ddbdf46b23d04ad9df05e9f6ce2a5
Jola-ade <ayilarajolaade@gmail.com>
2025-10-07 15:27:22 +0100

    Fix: adjust controlled component syntax to match ALX checker requirements

--- form-handling-react/src/components/RegistrationForm.jsx
+++ form-handling-react/src/components/RegistrationForm.jsx
@@ -1,34 +1,21 @@
 import { useState } from "react";
 
 function RegistrationForm() {
-  const [formData, setFormData] = useState({
-    username: "",
-    email: "",
-    password: "",
-  });
-
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   const [error, setError] = useState("");
-
-  const handleChange = (e) => {
-    setFormData({
-      ...formData,
-      [e.target.name]: e.target.value,
-    });
-  };
 
   const handleSubmit = (e) => {
     e.preventDefault();
 
-    // Basic validation
-    if (!formData.username || !formData.email || !formData.password) {
+    if (!username || !email || !password) {
       setError("All fields are required!");
       return;
     }
 
     setError("");
-    console.log("User Registered:", formData);
-
-    // Simulate API
+    console.log("User Registered:", { username, email, password });
     alert("User registered successfully!");
   };
 
@@ -42,24 +29,24 @@ function RegistrationForm() {
         type="text"
         name="username"
         placeholder="Username"
-        value={formData.username}
-        onChange={handleChange}
+        value={username}       
+        onChange={(e) => setUsername(e.target.value)}
       />
 
       <input
         type="email"
         name="email"
         placeholder="Email"
-        value={formData.email}
-        onChange={handleChange}
+        value={email}         
+        onChange={(e) => setEmail(e.target.value)}
       />
 
       <input
         type="password"
         name="password"
         placeholder="Password"
-        value={formData.password}
-        onChange={handleChange}
+        value={password}       
+        onChange={(e) => setPassword(e.target.value)}
       />
 
       <button type="submit">Register</button>
