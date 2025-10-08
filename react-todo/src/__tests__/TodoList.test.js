3628578b173b777e0f545edb7ed34fc0d4f4b688
Jola-ade <ayilarajolaade@gmail.com>
2025-10-09 00:16:19 +0100

    moved __tests folder__

--- react-todo/src/__tests__/TodoList.test.js
+++ react-todo/src/__tests__/TodoList.test.js
@@ -1,6 +1,6 @@
 import { render, screen, fireEvent, waitFor } from "@testing-library/react";
 import "@testing-library/jest-dom";
-import TodoList from "../TodoList";
+import TodoList from "../components/TodoList.jsx";
 
 test("renders initial todos", () => {
   render(<TodoList />);
